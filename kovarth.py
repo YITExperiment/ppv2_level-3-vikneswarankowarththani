8c0db875c4611f722d610177cf1a0f80c39a61b2
vikneswarankowarththani <135310666+vikneswarankowarththani@users.noreply.github.com>
2023-06-02 15:04:22 +0530

    Add files via upload

---
+++ kovarth.py
@@ -0,0 +1,21 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/USER-HP/Desktop/thanusha/kovarththani.py ==========
+hello, world!
+>>> 
+========== RESTART: C:/Users/USER-HP/Desktop/thanusha/kovarththani.py ==========
+Hello, world!
+what is your name? Kovarththani
+Hello  Kovarththani
+>>> age=12
+>>> print(age)
+12
+>>> weight=28
+>>> print(weight)
+28
+>>> height=130cm
+SyntaxError: invalid decimal literal
+>>> height=130
+>>> print(height)
+130
